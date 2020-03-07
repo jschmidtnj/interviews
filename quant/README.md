3d26c055551256895d983660401114bc3cf44c4e
Joshua <jschmidtnj@gmail.com>
2020-03-07 10:27:01 -0500

    verified solutions to quant. submitting now

--- quant/README.md
+++ quant/README.md
@@ -1,3 +1,3 @@
 # quant coding assignment
 
-in this directory there is a python file with my solutions to the quant coding assignment
+In this directory there is a python file with my solutions to the quant coding assignment. I didn't split the program into 3 separate files because I saw that instruction after I completed everything already. Sorry about that.
--- quant/quant.py
+++ quant/quant.py
@@ -19,6 +19,10 @@ class quant:
     trading_days_per_month = 20
 
     def __init__(self, drop_table=False):
+        """
+        class containing solutions to 3 parts of the questions provided
+        constructor for creating tables in sqlite and instantiating globals
+        """
         self.start_data = '2019-1-1'
         self.end_data = '2019-12-31'
         db_name = 'SSMIF.db'
@@ -43,11 +47,16 @@ class quant:
         self.conn.commit()
 
     def __del__(self):
+        """
+        destructor to close connection to sqlite
+        """
         self.conn.close()
 
     def Daily_Returns(self, adjusted_close_values):
         """
         return daily returns as percent
+        there is a built-in function in pandas to do this, but I didn't use
+        it because I didn't think that was the point of this question
         """
         res = pd.DataFrame(data={
             self.daily_returns_key: [0.0] * len(adjusted_close_values)
@@ -59,10 +68,16 @@ class quant:
         return res
 
     def validate_confidence_level(self, confidence_level):
+        """
+        checks for valid confidence level input
+        """
         if confidence_level < 0 or confidence_level > 1:
             raise ValueError('invalid confidence level given')
 
     def get_stock_data(self, ticker):
+        """
+        gets stock data for given ticker from yahoo finance
+        """
         try:
             return pdr.get_data_yahoo(ticker,
                                       start=self.start_data,
@@ -72,17 +87,26 @@ class quant:
             return
 
     def daily_to_monthly(self, daily):
+        """
+        converts daily statistic to monthly by multiplying by sqrt of trading days per month
+        """
         return daily * (self.trading_days_per_month ** 0.5)
 
     def monthly_to_daily(self, monthly):
+        """
+        converts monthly statistic to daily by dividing by sqrt of trading days per month
+        """
         return monthly / (self.trading_days_per_month ** 0.5)
 
     def Monthly_VaR(self, ticker, confidence_level=0.05, data=None):
         """
-        monte carlo returned
+        this calculates the monthly VaR for a given ticker at a certain ci
+        monte carlo simulation result returned
         parmetric and historical also calculated
+        monte carlo and parametric requires numpy for ppf and random numbers,
+        but historical can be calculated just with pandas stats library. monte carlo
+        gives the best results though (according to internet).
         """
-        num_sims = 1e6
         self.validate_confidence_level(confidence_level)
         if data is None:
             try:
@@ -124,6 +148,7 @@ class quant:
         #       .format(parametric, historical))
         # seed generator
         random.seed(50)
+        num_sims = 1e6
         simulated = random.normal(mean, std, int(num_sims))
         monte_carlo = self.daily_to_monthly(
             percentile(simulated, confidence_level * 100))
@@ -132,7 +157,7 @@ class quant:
 
     def Monthly_CVaR(self, ticker, confidence_level=0.05):
         """
-        average of the worst losses - under var
+        CVaR - average of the worst losses - under var
         """
         data = self.get_stock_data(ticker)
         var = self.monthly_to_daily(
@@ -142,12 +167,12 @@ class quant:
         return self.daily_to_monthly(returns[returns.lt(var)].mean())
 
     def Monthly_Volatility(self, ticker):
+        """
+        calculates volatility based on the returns over the course of a month
+        """
         data = self.get_stock_data(ticker)
         return self.daily_to_monthly(
             self.Daily_Returns(data[self.adj_close_key])[self.daily_returns_key].var() ** 0.5)
-
-    def date_to_timestamp(self, date):
-        return int(date.timestamp())
 
     def Fill_Table(self, ticker):
         data = self.get_stock_data(ticker)
@@ -158,7 +183,7 @@ class quant:
             '''.format(
                 self.table_name, self.timestamp_key, self.open_key,
                 self.high_key, self.low_key, self.close_key, self.adj_close_key),
-                (self.date_to_timestamp(data.index[i]),
+                (int(data.index[i].timestamp()),
                  data[self.open_key][i],
                  data[self.high_key][i],
                  data[self.low_key][i],
@@ -168,11 +193,15 @@ class quant:
         self.conn.commit()
 
     def _sum_single_list(self, list, mult, start, end):
+        """
+        helper - finds sum of a single list in the ssmif function
+        """
         res = 0
         found_start = False
         found_end = False
-        for i in range(len(list)):
-            if list[i] == start and end in list[i+1:]:
+        num_in_list = len(list)
+        for i in range(num_in_list):
+            if list[i] == start and i < num_in_list - 1 and end in list[i+1:]:
                 found_start = True
             if found_start and not found_end:
                 res += list[i] * mult
@@ -183,6 +212,9 @@ class quant:
         return res
 
     def sum_ssmif(self, nested_list):
+        """
+        finds special sum of nested list
+        """
         sums = []
         for i in range(len(nested_list)):
             sums.append(self._sum_single_list(nested_list[i], 2, 9, 6)
